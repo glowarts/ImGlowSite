0a36b3ec8a17773066bd480901f6795049ed1ec4
glowarts <44754533+glowarts@users.noreply.github.com>
2023-04-10 20:40:09 +0200

    AboutMe

--- gallery.js
+++ gallery.js
@@ -6,22 +6,23 @@ const popupClose = document.querySelector('.popup-close');
 
 // Open popup when clicking on a gallery item
 gallery.addEventListener('click', (event) => {
-    if (event.target.tagName === 'IMG') {
-      const title = event.target.nextElementSibling.querySelector('h3').textContent;
-      const description = event.target.nextElementSibling.querySelector('p').textContent;
-      const imageUrl = event.target.src;
+    const clickedItem = event.target.closest('.gallery-item');
+    if (clickedItem) {
+      const title = clickedItem.querySelector('h3').textContent;
+      const description = clickedItem.querySelector('p').textContent;
+      const imageUrl = clickedItem.querySelector('img').src;
       popupTitle.textContent = title;
       popupDescription.textContent = description;
       const popupImage = popup.querySelector('.popup-image');
       popupImage.src = imageUrl;
       popup.style.display = 'block';
-
+  
       // Close popup when clicking on the image
       popupImage.addEventListener('click', () => {
         popup.style.display = 'none';
       });
     }
-});
+  });
 
 // Close popup when clicking on the close button or outside the popup
 popupClose.addEventListener('click', () => {
